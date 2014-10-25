f588dafbacb568c63f7ebdf3029f515f22e912dc
Kevin Casey <kacasey@berkeley.edu>
2014-10-25 11:14:28 -0700

    Initial Commit

---
+++ studywithme/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  studywithme
+//
+//  Created by Kevin Casey on 10/25/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ studywithmeTests/studywithmeTests.m
@@ -0,0 +1,40 @@
+//
+//  studywithmeTests.m
+//  studywithmeTests
+//
+//  Created by Kevin Casey on 10/25/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface studywithmeTests : XCTestCase
+
+@end
+
+@implementation studywithmeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
