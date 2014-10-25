c2ed96aaa96b933f6b5a97a319eaf8a4373ac4b1
Kevin Casey <kacasey@berkeley.edu>
2014-10-25 11:57:39 -0700

    7.0?

---
+++ studywithme/CreateStudyGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateStudyGroupViewController.h
+//  studywithme
+//
+//  Created by Kevin Casey on 10/25/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateStudyGroupViewController : UIViewController
+
+@end
---
+++ studywithme/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  studywithme
+//
+//  Created by Kevin Casey on 10/25/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
