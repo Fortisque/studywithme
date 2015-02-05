aaa019e92898aca3e3c0cf94b71776c297330050
alice <aliceworkmail@gmail.com>
2015-02-05 15:20:32 -0800

    added add button

--- studywithme/CreateStudyGroupTableViewController.m
+++ studywithme/CreateStudyGroupTableViewController.m
@@ -23,6 +23,8 @@
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
+    
+    
     [self queryCourses];
 }
 
