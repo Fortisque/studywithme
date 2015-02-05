8942176d048a07497b9dd8b180c6700361babe88
alice <aliceworkmail@gmail.com>
2015-02-04 22:03:24 -0800

    added button

--- studywithme/StudyGroupsTableViewCell.h
+++ studywithme/StudyGroupsTableViewCell.h
@@ -7,11 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CreateStudyGroupTableViewController.h"
 
 @interface StudyGroupsTableViewCell : UITableViewCell
 @property (nonatomic, weak) IBOutlet UILabel *classNameLabel;
 @property (nonatomic, weak) IBOutlet UILabel *locationLabel;
 @property (nonatomic, weak) IBOutlet UILabel *timeLabel;
 @property (weak, nonatomic) IBOutlet UIButton *editButton;
+- (IBAction)editButtonPressed:(id)sender;
 
 @end
--- studywithme/StudyGroupsTableViewCell.m
+++ studywithme/StudyGroupsTableViewCell.m
@@ -31,4 +31,6 @@
     // Configure the view for the selected state
 }
 
+- (IBAction)editButtonPressed:(id)sender {
+}
 @end
