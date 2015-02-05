bb6e0fc5549b5476944f0a7e839d33a0ecdf6687
Kevin Casey <kacasey@berkeley.edu>
2015-02-05 10:13:02 -0800

    fixing linker to work with pods

---
+++ studywithme/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  studywithme
+//
+//  Created by Kevin Casey on 2/5/15.
+//  Copyright (c) 2015 ieor190. All rights reserved.
+//
+
+#import "JSQMessages.h"
+
+@interface MessagesViewController : JSQMessagesViewController
+
+@end
