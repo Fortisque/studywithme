b188613560bea017e606d3efd6151d21138f1aab
alice <aliceworkmail@gmail.com>
2015-03-08 16:25:32 -0700

    added feedback

---
+++ studywithme/FeedbackViewController.m
@@ -0,0 +1,37 @@
+//
+//  FeedbackViewController.m
+//  studywithme
+//
+//  Created by Alice Jia Qi Liu on 3/8/15.
+//  Copyright (c) 2015 ieor190. All rights reserved.
+//
+
+#import "FeedbackViewController.h"
+
+@interface FeedbackViewController ()
+
+@end
+
+@implementation FeedbackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
